30cb0bf5276bfa59e3cca96bdced3cece0b812c2
Lav Kushwaha <lavkumar062@gmail.com>
2026-02-03 14:05:04 +0530

    terraform for ec2 added

---
+++ Terraform_ec2/main.tf
@@ -0,0 +1,18 @@
+
+data "aws_security_group" "default" {
+  name = "default"
+}
+
+
+
+resource "aws_instance" "first-ec2-from-terraform" {
+  ami                    = "ami-0c02fb55956c7d316"
+  key_name               = var.key_name
+  vpc_security_group_ids = [
+    data.aws_security_group.default.id
+  ]
+  instance_type          = var.instance_type
+  tags = {
+    Name = "Task 2 of pearlthoughts"
+  }
+}
---
+++ Terraform_ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "instance_type" {
+  type = string
+}
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+variable "key_name" {
+  type = string
+}
+variable "image_name" {
+  type = string
+}
